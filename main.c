0b319cd6d9fef0ba5def65ae826e76896cb43bbb
Kyaw Zin  Thant <68962256+Maung-Kyaw@users.noreply.github.com>
2023-11-22 20:22:44 +0700

    final project ( parking lot)

--- main.c
+++ main.c
@@ -1,47 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_CAR 20
 
-struct parking{
+#define MAX_ROWS 5
+#define MAX_COLS 5
 
-} ;
+struct Car {
+    int number;
+};
 
-int main()
-{
+// Function prototypes
+void Park_Car(struct Car *parked_car, int parking_slot[MAX_ROWS][MAX_COLS]);
+void Remove_Car(int parking_slot[MAX_ROWS][MAX_COLS],struct Car *parked_car);
+void show_parking(int parking_slot[MAX_ROWS][MAX_COLS]);
 
+int main() {
+    struct Car parked_car;
+    int parking_slot[MAX_ROWS][MAX_COLS]={};
     int input_num;
 
-
-    do{
-        printf("1. Park a car\n2. Remove a car\n3. Show parking lot\n4. Exit the program");
+    do {
+        printf("\033[1;35m1. Park a car\n2. Remove a car\n3. Show parking lot\n4. Exit the program\033[0m");
         printf("\nChoose one :");
-        scanf("%d",&input_num);
+        scanf("%d", &input_num);
 
-        switch(input_num){
-        case 1:
-            printf("Park a car\n\n");
-            break;
-        case 2:
-            printf("Remove a car\n\n");
-            break;
-        case 3:
-            printf("Show parking lot\n\n");
-            break;
-        case 4:
-            printf("You have parked ur car successfully\n\n");
-            exit(1);
+        switch (input_num) {
+            case 1:
+                Park_Car(&parked_car, parking_slot);
+                break;
+            case 2:
+                Remove_Car(parking_slot,&parked_car);
+                break;
+            case 3:
+                show_parking(parking_slot);
+                break;
+            case 4:
+                printf("Thank you for using the parking!\n");
+                exit(1);
         }
-    }while(input_num<=4 && input_num>0);
+    } while (input_num <= 4 && input_num > 0);
 
+    return 0;
+}
 
+// Function definition
+void Park_Car(struct Car *parked_car, int parking_slot[MAX_ROWS][MAX_COLS]) {
+    int x, y;
+
+    do {
+        printf("Enter a row (1-5):");
+        scanf("%d", &x);
+        printf("Enter a column (1-5):");
+        scanf("%d", &y);
+
+        if (x >= 1 && x <= MAX_ROWS && y >= 1 && y <= MAX_COLS) {
+            if (parking_slot[x - 1][y - 1] == 0) {
+                printf("The parking slot is available!\n");
+                printf("Enter your car number:");
+                scanf("%d", &parked_car->number);
+                parking_slot[x - 1][y - 1] = parked_car->number;
+                printf("Parked car number is %d\n", parked_car->number);
+            } else {
+                printf("This parking slot is full. Choose another Col & Row\n");
+            }
+        } else {
+            printf("Invalid row or column. Please enter values between 1 and 5.\n");
+        }
+
+    } while (parking_slot[x - 1][y - 1] == 0);
+}
+void Remove_Car(int parking_slot[MAX_ROWS][MAX_COLS],struct Car *parked_car)
+{
+    int x,y;
+
+    printf("Enter the row to remove:");
+    scanf("%d",&x);
+    printf("Enter the column to remove:");
+    scanf("%d",&y);
+
+    if(parking_slot[x-1][y-1]==0)
+        printf("There is no car to remove!\n");
+    else
+        printf("Successfully removed! Car number is %d\n\n",parking_slot[x-1][y-1]);
+        parking_slot[x-1][y-1]=0;
 
 }
-// 4 functions
+void show_parking(int parking_slot[MAX_ROWS][MAX_COLS]){
+    int i,j;
+    for(i=0;i<MAX_ROWS;i++){
+        for(j=0;j<MAX_COLS;j++){
+            if(parking_slot[i][j]==0){
+                printf("\033[0;32m"); //green shows the available slots
+                printf("%5s","*");
+            }
+            else{
+                printf("\033[0;31m");
+                printf("%5s","*");
+            }
+        }
+    printf("\n");
+    }}
+
 /*
-void Park_Car(){
-}
-
 void Remove_Car(){
 
 }
@@ -51,4 +111,5 @@ void Show_Parking(){
 void exit(){
 
 }
+
 */
